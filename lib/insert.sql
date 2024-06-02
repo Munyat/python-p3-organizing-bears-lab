b287c3fa9d5dab990b8d73de968e8fee416de913
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2024-06-02 17:52:59 +0300

    complete bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 3, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 4, 'F', 'Blonde', 'Playful', 1),
+('Sergeant Brown', 6, 'M', 'Brown', 'Serious', 1),
+('Melissa', 2, 'F', 'Brown', 'Gentle', 1),
+('Grinch', 7, 'M', 'Green', 'Grumpy', 1),
+('Wendy', 3, 'F', 'White', 'Curious', 1),
+(NULL, 1, 'M', 'Black', 'Unknown', 1);
+
